8dcf9128b065251f2fbce898d01dc8daf2296391
nedaasadi93 <asadi.neda93@gmail.com>
2025-11-22 21:40:18 +0330

    Feat: Implement JwtService : define access and refresh secret

--- src/main/java/com/example/expensetracker/security/jwt/JwtService.java
+++ src/main/java/com/example/expensetracker/security/jwt/JwtService.java
@@ -1,9 +1,24 @@
 package com.example.expensetracker.security.jwt;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
+
+    @Value("${security.jwt.access.secret}")
+    private String accessSecret;
+
+    @Value("${security.jwt.refresh.secret}")
+    private String refreshSecret;
+
+    @Value("${security.jwt.access.expiration}")
+    private long accessExpiration;
+
+    @Value("${security.jwt.refresh.expiration}")
+    private long refreshExpiration;
+
+
     public void validateToken(String token, JwtTokenType accessToken) {
     }
 

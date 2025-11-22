99a36ad60217e95c44c1b78346cf257d0db67873
nedaasadi93 <asadi.neda93@gmail.com>
2025-11-23 01:12:22 +0330

    Feat: Fix Request mapping for users

--- src/main/java/com/example/expensetracker/user/controller/UserController.java
+++ src/main/java/com/example/expensetracker/user/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("${idn}")
+@RequestMapping("${rest.idn}")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;

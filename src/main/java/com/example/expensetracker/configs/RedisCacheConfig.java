edcc9cf98393c0ab14785b12a6911448d00a3d48
nedaasadi93 <asadi.neda93@gmail.com>
2025-11-23 00:37:48 +0330

    Feat: Add RedisCacheConfig

---
+++ src/main/java/com/example/expensetracker/configs/RedisCacheConfig.java
@@ -0,0 +1,21 @@
+package com.example.expensetracker.configs;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+@Configuration
+@EnableCaching
+public class RedisCacheConfig {
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig();
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(config)
+                .build();
+    }
+}
